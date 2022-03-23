a2051dbfd2bfab67b12de81ad8d927d1f908f6c1
sean <shhopeless@gmail.com>
2022-03-22 21:46:22 -0400

    C# Assignment

---
+++ C#/Assignment 1/02UnderstandingTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*1.Create a console application project named /02UnderstandingTypes/ that outputs the 
+number of bytes in memory that each of the following number types uses, and the minimum and maximum values they can have: 
+sbyte, byte, short, ushort, int, uint, long, ulong, float, double, and decimal.*/
+Console.WriteLine("sbyte Max Value:{0}, Min Value:{1}", sbyte.MaxValue, sbyte.MinValue);
+Console.WriteLine("byte Max Value:{0}, Min Value:{1}", byte.MaxValue, byte.MinValue);
+Console.WriteLine("short Max Value:{0}, Min Value:{1}", short.MaxValue, short.MinValue);
+Console.WriteLine("ushort Max Value:{0}, Min Value:{1}", ushort.MaxValue, ushort.MinValue);
+Console.WriteLine("int Max Value:{0}, Min Value:{1}", int.MaxValue, int.MinValue);
+Console.WriteLine("uint Max Value:{0}, Min Value:{1}", uint.MaxValue, uint.MinValue);
+Console.WriteLine("long Max Value:{0}, Min Value:{1}", long.MaxValue, long.MinValue);
+Console.WriteLine("ulong Max Value:{0}, Min Value:{1}", ulong.MaxValue, ulong.MinValue);
+Console.WriteLine("float Max Value:{0}, Min Value:{1}", float.MaxValue, float.MinValue);
+Console.WriteLine("double Max Value:{0}, Min Value:{1}", double.MaxValue, double.MinValue);
+Console.WriteLine("decimal Max Value:{0}, Min Value:{1}", decimal.MaxValue, decimal.MinValue);
+
+
+
+
+/*2.Write program to enter an integer number of centuries and convert it to years, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds.  
+ * Use an appropriate data type for every data conversion. Beware of overflows!*/
+
+int num;
+Console.WriteLine("Enter an integer number of centuries");
+//Centruies
+num = Convert.ToInt32(Console.ReadLine());
+
+//Years
+ushort numYears = (ushort)(num * 100);
+
+//Days
+decimal numDays = (decimal)(numYears * 365.24);
+
+//Hours
+uint numHours = (uint)(numDays * 24);
+
+//Minutes
+ulong numMinutes = numHours * 60;
+
+//Seconds
+ulong numSeconds = numMinutes * 60;
+
+//Milliseconds
+decimal numMilliseconds = (numSeconds * 1000);
+
+//Microseconds
+decimal numMicroseconds = (numMilliseconds * 1000);
+
+//Nanoseconds
+decimal numNanoseconds = (numMicroseconds * 1000);
+
+Console.WriteLine($"{num} Centruies = {numYears} Years = {numDays} Days = {numHours} Hours = {numMinutes} Minutes = {numSeconds} Seconds = {numMilliseconds} Milliseconds = {numMicroseconds} Microseconds = {numNanoseconds} Nanoseconds");
---
+++ C#/Assignment 1/Exercise03/Program.cs
@@ -0,0 +1,127 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* 1. FizzBuzz */
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+int max = 500; for (byte i = 0; i < max; i++) { Console.WriteLine(i); }
+// infinity loop 
+
+
+
+
+/* 2. print -a-Pyramid*/
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= 5 - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}
+
+
+/* 3.Random number and guess*/
+Random n = new Random();
+int rnum = n.Next(0, 4);
+Console.WriteLine("guess number between 1 and 3");
+int gnum = Convert.ToInt32(Console.ReadLine());
+
+if (Enumerable.Range(1, 3).Contains(gnum))
+{
+    if (gnum == rnum)
+    {
+        Console.WriteLine("correct");
+    }
+    if (gnum > rnum)
+    {
+        Console.WriteLine("high");
+    }
+    if (gnum < rnum)
+    {
+        Console.WriteLine("low");
+    }
+}
+else
+{
+    Console.WriteLine("out of range");
+}
+
+
+/*4.Write a simple program that defines a variable representing a birth date and calculates */
+
+DateTime Currentt = DateTime.Today;
+
+Console.WriteLine("Enter the month: ");
+int bMonth = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the day: ");
+int bDay = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the year: ");
+int bYear = int.Parse(Console.ReadLine());
+
+DateTime Born = new DateTime(bYear, bMonth, bDay);
+
+TimeSpan diff1 = Currentt.Subtract(Born);
+Console.WriteLine(diff1);
+
+
+/*5.Write a program that greets the user using the appropriate greeting for the time of day. */
+
+DateTime Current = DateTime.Now;
+int cHour = Current.Hour;
+int Morning = 6;
+int Afternoon = 12;
+int Evening = 18;
+int Night = 22;
+
+if (Morning <= cHour && cHour < Afternoon)
+{
+    Console.WriteLine("Good Morning");
+};
+
+if (Afternoon <= cHour && cHour < Evening)
+{
+    Console.WriteLine("Good Afternoon!");
+};
+
+if (Evening <= cHour && cHour < Night)
+{
+    Console.WriteLine("Good Evening!");
+};
+
+if (Night <= cHour || cHour < Morning)
+{
+    Console.WriteLine("Good Night!");
+};
+
+/*6.Write a program that prints the result of counting up to 24 using four different increments. */
+for (int i = 1; i <= 4; i += 1)
+{
+    for (int j = 0; j <= 24; j += i)
+    {
+        Console.Write($"{j},");
+    }
+
+    Console.WriteLine();
+}
